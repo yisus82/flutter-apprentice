1f087a24f3b81c8b5f014bb1dfbe015cea0de8cd
yisus82 <djalma8@gmail.com>
2021-11-08 09:58:24 +0100

    RayChat completed

--- ray_chat/lib/main.dart
+++ ray_chat/lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../data/message_dao.dart';
+import '../data/user_dao.dart';
+import 'ui/login.dart';
 import 'ui/message_list.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // TODO: Add Firebase Initialization
+  await Firebase.initializeApp();
   runApp(const App());
 }
 
@@ -12,14 +18,31 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Add MultiProvider
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'RayChat',
-      theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
-      // TODO: Add Consumer<UserDao> here
-      home: const MessageList(),
-      // TODO: Add closing parenthesis
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<UserDao>(
+          lazy: false,
+          create: (_) => UserDao(),
+        ),
+        Provider<MessageDao>(
+          lazy: false,
+          create: (_) => MessageDao(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'RayChat',
+        theme: ThemeData(primaryColor: const Color(0xFF3D814A)),
+        home: Consumer<UserDao>(
+          builder: (context, userDao, child) {
+            if (userDao.isLoggedIn()) {
+              return const MessageList();
+            } else {
+              return const Login();
+            }
+          },
+        ),
+      ),
     );
   }
 }
--- ray_chat/lib/ui/message_list.dart
+++ ray_chat/lib/ui/message_list.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../data/message.dart';
+import '../data/message_dao.dart';
+import '../data/user_dao.dart';
+import 'message_widget.dart';
 
 class MessageList extends StatefulWidget {
   const MessageList({Key? key}) : super(key: key);
@@ -12,26 +18,32 @@ class MessageList extends StatefulWidget {
 class MessageListState extends State<MessageList> {
   final TextEditingController _messageController = TextEditingController();
   final ScrollController _scrollController = ScrollController();
-  // TODO: Add Email String
+  String? email;
 
   @override
   Widget build(BuildContext context) {
     WidgetsBinding.instance!.addPostFrameCallback((_) => _scrollToBottom());
-    // TODO: Add MessageDao
-
-    // TODO: Add UserDao
+    final messageDao = Provider.of<MessageDao>(context, listen: false);
+    final userDao = Provider.of<UserDao>(context, listen: false);
+    email = userDao.email();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('RayChat'),
-        // TODO: Replace with actions
+        actions: [
+          IconButton(
+            onPressed: () {
+              userDao.logout();
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // TODO: Add Message DAO to _getMessageList
-            _getMessageList(),
+            _getMessageList(messageDao),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -42,8 +54,7 @@ class MessageListState extends State<MessageList> {
                       keyboardType: TextInputType.text,
                       controller: _messageController,
                       onSubmitted: (input) {
-                        // TODO: Add Message DAO 1
-                        _sendMessage();
+                        _sendMessage(messageDao);
                       },
                       decoration:
                           const InputDecoration(hintText: 'Enter new message'),
@@ -55,8 +66,7 @@ class MessageListState extends State<MessageList> {
                         ? CupertinoIcons.arrow_right_circle_fill
                         : CupertinoIcons.arrow_right_circle),
                     onPressed: () {
-                      // TODO: Add Message DAO 2
-                      _sendMessage();
+                      _sendMessage(messageDao);
                     })
               ],
             ),
@@ -66,18 +76,46 @@ class MessageListState extends State<MessageList> {
     );
   }
 
-  // TODO: Replace _sendMessage
-  void _sendMessage() {
+  void _sendMessage(MessageDao messageDao) {
+    if (_canSendMessage()) {
+      final message = Message(
+        text: _messageController.text,
+        date: DateTime.now(),
+        email: email,
+      );
+      messageDao.saveMessage(message);
+      _messageController.clear();
+      setState(() {});
+    }
   }
 
-  // TODO: Replace _getMessageList
-  Widget _getMessageList() {
-    return const SizedBox.shrink();
+  Widget _getMessageList(MessageDao messageDao) {
+    return Expanded(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: messageDao.getMessageStream(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return const Center(child: LinearProgressIndicator());
+
+          return _buildList(context, snapshot.data!.docs);
+        },
+      ),
+    );
   }
 
-  // TODO: Add _buildList
+  Widget _buildList(BuildContext context, List<DocumentSnapshot>? snapshot) {
+    return ListView(
+      controller: _scrollController,
+      physics: const BouncingScrollPhysics(),
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot!.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
 
-  // TODO: Add _buildListItem
+  Widget _buildListItem(BuildContext context, DocumentSnapshot snapshot) {
+    final message = Message.fromSnapshot(snapshot);
+    return MessageWidget(message.text, message.date, message.email);
+  }
 
   bool _canSendMessage() => _messageController.text.length > 0;
 
